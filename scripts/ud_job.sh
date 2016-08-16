f93fa47a905bd77770d8df908156289873bc95cf
Mark Zaslavskiy <mark.zaslavskiy@gmail.com>
2016-08-16 11:08:27 +0300

    #7313: adds argcount check + small refactoring

--- scripts/ud_job.sh
+++ scripts/ud_job.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [ "$#" -ne 3 ]; then
+	echo "Illegal number of parameters"
+	echo "Usage: <path>/scripts/ud_job.sh <jenkins config path> [update|delete] <yaml file path>"
+fi
+
 JENKINS_CONFIG=$1
 ACTION=$2 # update/ delete 
 YAML_JOB=$3
@@ -8,13 +13,14 @@ cp ${YAML_JOB} ${YAML_JOB}"_temp"
 
 TIMESTAMP=$(date --rfc-3339=seconds)
 GIT_ID=$(git rev-parse HEAD)
+TAG="[${TIMESTAMP} ID=${GIT_ID}]"
 echo "Timestamp: "${TIMESTAMP}", Git ID: "${GIT_ID}
 
 if grep -q "^    description:\s*" ${YAML_JOB}"_temp"; then
-  sed -i -e "s/^    description[^']*'[^']*/& \n\r[${TIMESTAMP} ID=${GIT_ID}]\n\r/" ${YAML_JOB}_temp
+  sed -i -e "s/^    description[^']*'[^']*/& \n\r${TAG}\n\r/" ${YAML_JOB}_temp
   echo "description modified" 
 else
-  sed -i -e "3i\ \ \ \ description:  '[${TIMESTAMP} ID=${GIT_ID}]\n\r'" ${YAML_JOB}_temp
+  sed -i -e "3i\ \ \ \ description:  '${TAG}\n\r'" ${YAML_JOB}_temp
   echo "description added"
 fi
 
