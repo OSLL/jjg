c6d64a1f5743ffeb406c527c50cee27328af02c5
Tatiana Berlenko <tatiana_berlenko@epam.com>
2017-02-01 17:00:07 +0300

    7837_display_path_to_yaml_in_jjd

--- scripts/ud_job.sh
+++ scripts/ud_job.sh
@@ -15,13 +15,17 @@ cp ${YAML_JOB} ${YAML_JOB}"_temp"
 TIMESTAMP=$(date --rfc-3339=seconds)
 GIT_ID=$(git rev-parse HEAD)
 TAG="[${TIMESTAMP} ID=${GIT_ID}]"
+GIT_BRANCH=$(git branch | grep '*' | sed 's/[*]//')
+PATH_AND_BRANCH_TAG="[PATH=${YAML_JOB} GIT BRANCH=${GIT_BRANCH}]"
+
 echo "Timestamp: "${TIMESTAMP}", Git ID: "${GIT_ID}
 
 if grep -q "^    description:\s*" ${YAML_JOB}"_temp"; then
-  sed -i -e "s/^    description[^']*'[^']*/& \n\r${TAG}\n\r/" ${YAML_JOB}_temp
+  sed -i -e "s/^    description[^']*'[^']*/& \n\r${TAG}\n\r${PATH_AND_BRANCH_TAG=}\n\r/" ${YAML_JOB}_temp
+  
   echo "description modified" 
 else
-  sed -i -e "3i\ \ \ \ description:  '${TAG}\n\r'" ${YAML_JOB}_temp
+  sed -i -e "3i\ \ \ \ description:  '${TAG}\n\r${PATH_AND_BRANCH_TAG=}\n\r'" ${YAML_JOB}_temp
   echo "description added"
 fi
 
