1c38a57d49b8711ee791499d07efbcac2011e87d
Ilfat Kinyaev <kinyaevilfat@gmail.com>
2017-08-08 13:42:59 +0300

    8505 realized add job to view feature

--- scripts/ud_job.sh
+++ scripts/ud_job.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+function prop {
+    grep "${1}=" ${JENKINS_CONFIG}|cut -d'=' -f2
+}
+
+function add_job_to_view {
+  JOB_NAME=`echo $YAML_JOB | grep -o -P '(?<=\w\/)\w*(?=.yaml)'`
+  if ! java -jar "$HOME/jenkins-cli.jar" -s $(prop 'url') -auth $(prop 'user'):$(prop 'password') add-job-to-view "${1}" "$JOB_NAME"; then
+    echo "ERROR: job - '$JOB_NAME' adding to view '${1}' failed!"
+  else
+    echo "SUCCESS: job - '$JOB_NAME' has been added to view - '${1}'"
+  fi
+}
+
 if [ "$#" -ne 3 ]; then
 	echo "Illegal number of parameters"
 	echo "Usage: <path>/scripts/ud_job.sh <jenkins config path> {update|delete} <yaml file path>"
@@ -31,6 +44,12 @@ fi
 
 jenkins-jobs --conf ${JENKINS_CONFIG} ${ACTION} ${YAML_JOB}_temp
 res=$?
+
+view_name=`grep "description:" ${YAML_JOB}"_temp" | grep -o -P '(?<=View:)\s*(\w*)(?=.)' | grep -o -P '[^\s]+'`
+
+if [[ -n "${view_name// }" ]]; then
+	add_job_to_view $view_name
+fi
 
 cat  ${YAML_JOB}_temp
 
